4b7e5ebf9b70a029a09f1b2999c3a9d29bbb0f3e
Patrick Kearns <kearnspj23@gmail.com>
2023-11-09 20:45:47 -0500

    added antismash

--- PFC_pangenome_analysis.R
+++ PFC_pangenome_analysis.R
@@ -61,22 +61,29 @@ ggplot(dst.tbl) +
 
 #hierarchical cluster data
 clst.blast <- bClust(dst.tbl, linkage = "complete", threshold = 0.75)
+clst.blast2<-as.data.frame(clst.blast)
 
 #construct pangenome matrix
 panmat.blast <- panMatrix(clst.blast)
+panmat.blast2<-as.data.frame(panmat.blast)
+
+panmat.blast3<-as.matrix(panmat.blast)
 
 #visualize the number of clusters per genomes
 tibble(Clusters = as.integer(table(factor(colSums(panmat.blast > 0),
                                           levels = 1:nrow(panmat.blast)))),
        Genomes = 1:nrow(panmat.blast)) %>% 
   ggplot(aes(x = Genomes, y = Clusters)) +
-  geom_col() + labs(title = "Number of clusters found in 1, 2,...,all genomes")
+  geom_col() +
+  ylab("Genes")+
+  xlab("Number of Genomes")
 
 #calculate pangenome size
 heaps.est <- heaps(panmat.blast, n.perm = 500)
 print(heaps.est)
 # Intercept      alpha 
 #811.142958   1.141321
+#alpha is above 1, so pangenome is closed
 print(chao(panmat.blast))
 #9302
 
@@ -86,10 +93,11 @@ print(fitted$BIC.tbl)
 ncomp <- 4
 fitted$Mix.tbl %>% 
   filter(Components == ncomp) %>% 
+  mutate(Single = Mixing.proportion * Detection.prob) %>%
   ggplot() +
-  geom_col(aes(x = "", y = Mixing.proportion, fill = Detection.prob)) +
+  geom_col(aes(x = "", y = Single, fill = Detection.prob)) +
   coord_polar(theta = "y") +
-  labs(x = "", y = "", title = "Pan-genome gene family distribution",
+  labs(x = "", y = "", title = "Average genome gene-family distribution",
        fill = "Detection\nprobability") +
   scale_fill_gradientn(colors = c("pink", "orange", "green", "cyan", "blue"))
 
@@ -98,7 +106,7 @@ library(ggdendro)
 d.man <- distManhattan(panmat.blast)
 ggdendrogram(dendro_data(hclust(d.man, method = "average")),
              rotate = TRUE, theme_dendro = FALSE) +
-  labs(x = "Genomes", y = "Unweighted Manhattan distance", title = "Pan-genome dendrogram")
+  labs(x = "Genomes", y = "Unweighted Manhattan distance")
 
 #view clustered genomes (manhattan weighted)
 pm <- panmat.blast                                                   # make a copy
@@ -108,7 +116,7 @@ distManhattan(pm, weights = weights) %>%
   hclust(method = "average") %>% 
   dendro_data() %>% 
   ggdendrogram(rotate = TRUE, theme_dendro = FALSE) +
-  labs(x = "Genomes", y = "Weighted Manhattan distance", title = "Pan-genome dendrogram")
+  labs(x = "Genomes", y = "Weighted Manhattan distance")
 
 
 #rarefaction of pangenome
@@ -120,8 +128,75 @@ rar.tbl %>%
   ggplot(aes(x = Genome, y = Clusters, group = Permutation)) +
   geom_line()
 
+#calculate average pangenome fluidity
+pg_fluid<-fluidity(panmat.blast, n.sim = 10000)
+#Mean        Std 
+#0.07105227 0.03486338
+#For example, a genomic fluidity of 0.1 represents that a pair of genomes have on average 10% unique genes and share 90% of their genes.
+
+#calculate gene weights
+weighty<-geneWeights(panmat.blast)
+hist(weighty)
+
 ###Read in panaroo pan matrix
 library(readr)
 gene_presence_absence <- read_delim("PFC_panaroo_results/gene_presence_absence.Rtab", delim = "\t", escape_double = FALSE, trim_ws = TRUE)
 
-colSums(gene_presence_absence[,-1])
+#read in KEGG annotations of genes
+kegg_annotations <- read.delim("~/GitHub/Pseudo_fluor/kegg_annotations.txt")
+kegg_annotations$KO<-trimws(kegg_annotations$KO)
+
+#read in KEGG DB
+ko_db<-read.delim("full_kegg.txt", header=T)
+ko_db$KO<-trimws(ko_db$KO)
+
+#create table of core genes (genes >19 per row)
+gene_presence_absence$sum<-rowSums(gene_presence_absence[,-1])
+core_genes<-gene_presence_absence[-which(gene_presence_absence$sum<=18),]
+shell_genes<-gene_presence_absence[which(gene_presence_absence$sum<=18),]
+shell_genes$Type<-"Shell"
+core_genes$Type<-"Core"
+
+
+#reshape data
+library(dplyr)
+library(plyr)
+library(reshape2)
+core_m<-melt(core_genes[,-21])
+shell_m<-melt(shell_genes[,-21])
+
+#append KO information
+core_m<-merge(core_m, kegg_annotations, by='Gene', all.y=F)
+shell_m<-merge(shell_m, kegg_annotations, by='Gene', all.y=F)
+
+#append KO DB annotations
+core_m<-merge(core_m, unique(ko_db), by.x='KO', by.y='KO', all.y=F, all.x=T)
+shell_m<-merge(shell_m, unique(ko_db), by.x='KO', by.y='KO', all.y=F, all.x=T)
+
+#summarize Level 3 kegg for each
+core_m<-ddply(core_m, c("Level2", 'Type'), summarize, sum=sum(value))
+names(core_m)<-c("Level3", "Type", "Total_core")
+shell_m<-ddply(shell_m, c("Level2", 'Type'), summarize, sum=sum(value))
+names(shell_m)<-c("Level3", "Type", "Shell_core")
+
+#merge data sets
+gene_sets<-merge(core_m, shell_m, by='Level3')
+gene_sets$Total<-gene_sets$Total_core+gene_sets$Shell_core
+
+ggplot(gene_sets, aes(Level3, Total_core))+
+  geom_bar(stat='identity')+
+  scale_y_log10()+
+  coord_flip()
+ggplot(gene_sets, aes(Level3, Shell_core))+
+  geom_bar(stat='identity')+
+  scale_y_log10()+
+  coord_flip()
+
+##########################################
+#read in break down of gene shells
+summary_statistics <- read.delim("~/GitHub/Pseudo_fluor/PFC_panaroo_results/summary_statistics.txt", header=FALSE)
+ggplot(summary_statistics[-5,], aes(x="", y=V3, fill=V2))+
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
+
+
